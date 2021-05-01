4e54865ad508997d81bbc22a3ebf8306da82cecc
teanderson <tec.anderson.ns@gmail.com>
2021-05-01 13:55:27 -0300

    api request e  finalizado mais ou menos a home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,10 +2,22 @@ import Head from "next/head";
 import Image from "next/image";
 import { Header } from "../componentes/Header";
 
-export default function Home() {
+export default function Home(props) {
   return (
-    <main>
-      <Header />
-    </main>
+    <div>
+       <p>{JSON.stringify(props.episodes)}</p>
+    </div>
   );
+}
+
+export async function getStaticProps() {
+  const response = await fetch("http://localhost:3333/episodes");
+  const data = await response.json();
+
+  return {
+    props: {
+      episodes: data,
+    },
+    revalidate: 60 * 60 * 8,
+  };
 }

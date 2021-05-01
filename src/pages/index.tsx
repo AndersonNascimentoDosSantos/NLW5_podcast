d9e4227c78b57558293e0ec204b57abd20952eed
teanderson <tec.anderson.ns@gmail.com>
2021-04-30 22:17:06 -0300

    static header

---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import Head from "next/head";
+import Image from "next/image";
+import { Header } from "../componentes/Header";
+
+export default function Home() {
+  return (
+    <main>
+      <Header />
+    </main>
+  );
+}
